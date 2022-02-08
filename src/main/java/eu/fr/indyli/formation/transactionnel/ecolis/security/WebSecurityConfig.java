052d0a51fb4a93bc08d656c5df1c14002975ead5
ZOME Christophe <christophe.zome@indyli-services.com>
2022-02-08 14:52:36 +0000

    Suppression de l'authentification

--- src/main/java/eu/fr/indyli/formation/transactionnel/ecolis/security/WebSecurityConfig.java
+++ src/main/java/eu/fr/indyli/formation/transactionnel/ecolis/security/WebSecurityConfig.java
@@ -39,7 +39,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http.cors().and().csrf().disable().authorizeRequests()
         .antMatchers("/token/*", "/token/is-token-valid", "/signup").permitAll()
-        .anyRequest().authenticated()
+        //.anyRequest().authenticated()
         .and()
         .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
